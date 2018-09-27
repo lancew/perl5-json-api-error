dccdf149d815e181554d9fece98b39b41bfa2201
Paul Williams <kwakwaversal@gmail.com>
2018-09-27 21:24:41 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+JSON::API::Error - Blah blah blah
+
+# SYNOPSIS
+
+    use JSON::API::Error;
+
+# DESCRIPTION
+
+JSON::API::Error is
+
+# AUTHOR
+
+Paul Williams <kwakwa@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2018- Paul Williams
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/JSON/API/Error.pm
@@ -0,0 +1,39 @@
+package JSON::API::Error;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+JSON::API::Error - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use JSON::API::Error;
+
+=head1 DESCRIPTION
+
+JSON::API::Error is
+
+=head1 AUTHOR
+
+Paul Williams E<lt>kwakwa@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2018- Paul Williams
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
